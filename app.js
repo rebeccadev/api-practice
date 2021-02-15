6559e3ecddf61ecb08d87003d7db6c9dad024f87
rebeccadev <rebecca.redfern@hotmail.co.uk>
2021-02-15 18:03:01 +0000

    Add JS

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 // DOM objects
 const mainScreen = document.querySelector('.main-screen');
 const mountName = document.querySelector('.mount-name');
-const mountId = document.querySelector('.mount-id');
+const mountIdNumber = document.querySelector('.mount-id');
 const mountFrontImage = document.querySelector('.mount-front-image');
 const mountWeight = document.querySelector('.mount-weight');
 const mountHeight = document.querySelector('.mount-height');
 const MountDescription = document.querySelector('.mount-description');
 const tooltip = document.querySelector('.tooltip');
 const mountListItems = document.querySelectorAll('.list-item');
+const leftButton = document.querySelector('.left-button')
+const rightButton = document.querySelector('.right-button')
 console.log(mountListItems);
 
+// hard coded initial fetch 
 
-fetch('https://ffxivcollect.com/api/mounts/1')
+/*fetch('https://ffxivcollect.com/api/mounts/1')
     .then(res => {
         return   res.json();
     })
@@ -28,9 +31,28 @@ fetch('https://ffxivcollect.com/api/mounts/1')
 
         mountFrontImage.src = data['image'];
 
-    });
+    }); */
 
 
+
+
+// function 
+
+const handleRightButtonClick = (e) => {
+    console.log(e);
+};
+
+const handleListItemClick = (e) => {
+    if (!e.target) return;
+
+    const listItem = e.target;
+    if (!listItem.textContent) return;
+
+    const mountId = listItem.textContent.split('.')[0];
+    fetchMountData(mountId);
+
+
+};
 
 // Get data for right side of screen
 
@@ -42,8 +64,23 @@ fetch('https://ffxivcollect.com/api/mounts')
     .then(data => {
         console.log(data);
         const { results } = data;
+        results.reverse();
+        console.log(results);
 
-        console.log(results)
+// create new array
+const mountListResults = results.map 
+console.log(mountListResults);
+
+
+// create seperate array of ids 
+const mountId = results.map(results => results.id);
+
+// current page 
+
+let currentPage = mountId.slice(0, 1);
+
+
+console.log(currentPage); 
 
         for (let i = 0; i < mountListItems.length ; i++) {
             const mountListItem = mountListItems[i];
@@ -51,8 +88,9 @@ fetch('https://ffxivcollect.com/api/mounts')
 
 
             if (resultData) {
-                const { name } = resultData;
-                mountListItem.textContent = (name);
+                const { name, id} = resultData;
+                
+                mountListItem.textContent = id + '. ' + name;
             } else {
                 mountListItem.textContent = '';
             }
@@ -60,3 +98,35 @@ fetch('https://ffxivcollect.com/api/mounts')
 
     });
 
+
+const fetchMountData = mountId => {
+    fetch(`https://ffxivcollect.com/api/mounts/${mountId}`)
+    .then(res => {
+        return   res.json();
+    })
+    .then(data => {
+        console.log(data);
+        mainScreen.classList.remove('hide');
+        mountName.textContent = data['name'];
+        mountIdNumber.textContent = '#' + data['id'].toString().padStart(3,0,0);
+        mountWeight.textContent = data['movement'];
+        mountHeight.textContent = data['seats'];
+        MountDescription.textContent = data['description'];
+        tooltip.textContent = data['tooltip'];
+
+
+        mountFrontImage.src = data['image'];
+
+    });
+}
+
+
+
+// add event listener 
+
+
+rightButton.addEventListener('click', handleRightButtonClick); 
+
+for (const mountListItem of mountListItems) {
+    mountListItem.addEventListener('click', handleListItemClick);
+}
